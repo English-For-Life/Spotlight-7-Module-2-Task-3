4419aca224a675bc74e022d7c359fea571aa2df8
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-13 17:47:24 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"He waited in the airport ...... the plane arrived.",
+        options:["until", "as soon as"],
+        answer: 0
+    },
+    {
+        q:"Marry called her mother ...... she got home.",
+        options:["until", "as soon as"],
+        answer: 1
+    },
+    {
+        q:"He tried to find a good job ...... he graduated.",
+        options:["after", "until"],
+        answer: 0
+    },
+    {
+        q:"She was in bed ...... the phone rang.",
+        options:["after", "when"],
+        answer: 1
+    },
+    {
+        q:"He watched TV ...... he had dinner.",
+        options:["after", "when"],
+        answer: 0
+    }
+
+
+
+]
